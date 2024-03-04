60dc1af8cc4acc36a4585fb580a1fdb0e42ad6a9
Jason Ky <744169@student.dusd.net>
2024-03-04 10:01:08 -0800

    heck yeah
    i finisheed and i le3arned somucch like the bonuses were CRAZYYY

--- Unit1PlayerControlKyP7/Assets/Scripts/FollowPlayer.cs
+++ Unit1PlayerControlKyP7/Assets/Scripts/FollowPlayer.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject car;
-    Vector3 caroffset = new Vector3(0, 6, -10);
+    Vector3 caroffset = new Vector3(0, 6, -10); 
+    Vector3 caroffset2 = new Vector3(0, 3.5f, 1);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = car.transform.position + caroffset;
     }
 
     // Late Update is called on the frame after update.
@@ -18,5 +19,14 @@ public class FollowPlayer : MonoBehaviour
     {
         //offsets camera
         transform.position = car.transform.position + caroffset;
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (caroffset != caroffset2)
+            caroffset = caroffset2;
+            else
+            caroffset = new Vector3(0,6, -10);
+        
+        }
     }
 }
